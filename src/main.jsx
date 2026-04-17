5718fb4a162663938adfe90e3a102c16d83ecba3
Swarna-Saha324 <mumusaha85841@gmail.com>
2026-04-17 11:13:00 +0600

    Toastify part is centered

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {ToastContainer } from 'react-toastify'
-import Navbar from './components/Navber'
-import {  Outlet } from "react-router";
-import { RouterProvider } from "react-router/dom"
+import { ToastContainer, Bounce } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
+
+import { RouterProvider } from "react-router"
 import { router } from './router/Router'
-import Footer from './components/Footer'
 import { FriendProvider } from './context/FriendContext';
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   
     <FriendProvider>
-      <ToastContainer />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
       <RouterProvider router={router} />
     </FriendProvider>
   </StrictMode>,
